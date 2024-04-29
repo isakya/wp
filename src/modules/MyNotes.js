7b27625c49cfb1ef440b9df359d95d1311286e99
isakya <1016361268@qq.com>
2024-04-30 01:34:26 +0800

    点击编辑按钮，将文本变为可编辑状态

--- src/modules/MyNotes.js
+++ src/modules/MyNotes.js
@@ -6,9 +6,16 @@ class MyNotes {
 
     events() {
         $('.delete-note').on('click', this.deleteNote)
+        $('.edit-note').on('click', this.editNote)
     }
 
     // method will go here
+    editNote(e) {
+        var thisNote = $(e.target).parents('li');
+        thisNote.find('.note-title-field, .note-body-field').removeAttr('readonly').addClass('note-active-field')
+        thisNote.find('.update-note').addClass('update-note--visible')
+    }
+
     deleteNote(e) {
         var thisNote = $(e.target).parents('li');
         $.ajax({
